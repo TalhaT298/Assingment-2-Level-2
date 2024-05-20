948519d2936e9f05f650d1a298da9653751d505d
Talha <mdtalha2008@gmail.com>
2024-05-20 21:32:23 +0600

    basic setup done

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,23 @@
 import mongoose from "mongoose";
 import app from "./app";
+import config from "./config";
+
 
 async function main() {
-    await mongoose.connect("mongodb+srv://assignment-2:qj0Wf1Evnx4TgetE@cluster0.auvs2t0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+
+    try{
+        await mongoose.connect(config.database_url as string);
     
-    const port=5000;
-    app.listen(port, () => {
-        console.log(`Example app listening on port ${port}`)
+    
+    app.listen(config.port, () => {
+        console.log(`Example app listening on port ${config.port}`)
       })
+        
+    }catch(err){
+        console.log(err)
+    }
+
+    
   }
 
   main()
